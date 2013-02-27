c705752c9dbe810fcd1b43e534c2f29d1a41f5df
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-02-27 14:44:41 +0000

    new structure first pass

--- test/unit-queue.js
+++ test/unit-queue.js
@@ -1,36 +1,58 @@
-var Queue = require("../lib/queue");
+var Queue = require("../lib/queue"),
+    Job = require("../lib/job"),
+    util = require("util");
 
 // describe("Queue", function(){
 //     it("queue synopsis", function(){
-        var queue = new Queue();
+    
+        var queue = new Queue({ name: "Main Queue" })
+            .on("queue-starting", function(state){ console.log("Queue starting: " + state); })
+            .on("queue-complete", function(state){ console.log("Queue complete: " + state); })
+            .on("job-starting", function(state){ console.log("Job starting: " + state); })
+            .on("job-progress", function(state, progress){ 
+                console.log("Job starting: " + state + progress.percentComplete); 
+            })
+            .on("job-complete", function(state){ console.log("Job complete: " + state); })
+            .on("job-success", function(state){ console.log("Job success: " + state); })
+            .on("job-fail", function(state){ console.log("Job fail: " + state); })
+            .on("job-info", function(state, msg){ console.log("Job info: " + msg + state); })
+            .on("job-warning", function(state, msg){ console.log("job-warning: " + msg + state); })
+            .on("job-error", function(state, err){ console.log("job-error: " + state + err); })
+            .on("job-terminated", function(state){ console.log("job-terminated: " + state); });
 
-        ["file", "another file", "third file"].forEach(function(file){
-            var job = queue.add("process: " + file);
-            job.command = function(){
-               console.log("processing: " + file);
-               this.emitComplete();
-            };
-            job.on("terminated", function(){
-               console.log("undoing");
+        ["Dave", "Alan", "Geoff", "Mohammad", "Jesus", "Mandy"].forEach(function(person){
+            var job = new Job({
+                name: util.format("%s job application", person),
+                command: function(){
+                   if (Math.floor(Math.random() * 2)){
+                       this.emitSuccess();
+                   } else {
+                       this.emitFail();
+                   }
+                }
             });
-            job.on("complete", function(){
-                console.log("complete: " + file);
-                setTimeout(function(){
-                    console.log("easy dred");
-                }, 100);
+
+            job.onSuccess.add({
+                name: util.format("notify %s of success", person),
+                command: function(){
+                    console.log("SUCCESS, %s!", person);
+                    this.emitSuccess();
+                }
             });
+            
+            job.onFail.add({
+                name: util.format("notify %s of failure", person),
+                command: function(){
+                    console.log("FAILED, %s!", person);
+                    this.emitSuccess();
+                }
+            });
+
+            queue.add(job);
         });
-        
-        var job = new Job()
-        var onCompleteSubQueue = new Queue();
-        onCompleteSubQueue.add(subJob);
-        onCompleteSubQueue.add(subJob);
-        sub.on
-        
-        queue.add(job, onCompleteSubQueue)
 
+        queue.print();
         queue.start();
-        queue.print() // prints tree hierachy of all jobs with in-place updating of progress
 //    });
 // });
 
@@ -41,7 +63,6 @@ execute jobs
  in sequence or 
  a group of jobs in parallel
  
-*/
 [
     { name: "job 1", command: func }, 
     { name: "job 2", command: func },
@@ -58,3 +79,4 @@ execute jobs
     { name: "job 8", command: func },
     { name: "job 8", command: func, async: true }
 ]
+*/
