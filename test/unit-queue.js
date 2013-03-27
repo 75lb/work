677fabf6d8dfb2d82c5350721e51b32b1f3ded7b
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-03-27 22:20:31 +0000

    monitor

--- test/unit-queue.js
+++ test/unit-queue.js
@@ -12,7 +12,7 @@ var findNewJob = new Job({
     args: "Let's do this."
 });
 
-[1,2,3,4,5,6,7,8,9,10].forEach(function(index){
+[1].forEach(function(index){
     findNewJob.add({
         name: "job application " + index,
         parallel: true,
@@ -20,34 +20,34 @@ var findNewJob = new Job({
             var self = this, 
                 delay = 500-(index*20);
             setTimeout(function(){
-                l("application for job number: %d, delay: %d", index, delay);
+                // l("application for job number: %d, delay: %d", index, delay);
                 if (parseInt(Math.random()*2)){
                     self.success();
                 } else {
                     self.fail();
                 }
             }, delay);
-        }
-        ,children: [
+        },
+        children: [
             {
                 name: "celebrate " + index,
                 runOn: "success",
-                commandSync: l,
+                commandSync: String,
                 args: "How could they refuse. "
             },
             {
                 name: "sulk " + index,
                 runOn: "fail",
-                commandSync: l,
+                commandSync: String,
                 args: "I'm fucked."
             }
         ]
     });
 });
 
-findNewJob.on("complete", function(){
-    l("find a new job complete");
+findNewJob.on("monitor", function(job){
+    l("%s, running: %s, successful: %s, complete: %s", job.name, job.running, job.successful, job.complete);
 });
 
 findNewJob.run();
-
+// findNewJob.print();
