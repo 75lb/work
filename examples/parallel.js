bbd98dd416aaeb1786ebff68db20431a116623d0
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-05-19 23:28:41 +0100

    wip

--- examples/parallel.js
+++ examples/parallel.js
@@ -8,74 +8,45 @@ function delay(time){
     }, time);
 }
 
-// var main = new Job({ name: "main" }).add([
-//     {
-//         name: "1", 
-//         parallel: true,
-//         command: delay,
-//         args: 100,
-//         children: [
-//             {
-//                 name: "1.1", 
-//                 parallel: true,
-//                 command: delay,
-//                 args: 400,
-//             },
-//             {
-//                 name: "1.2",
-//                 parallel: true,
-//                 command: delay,
-//                 args: 50,
-//                 children: [
-//                     {
-//                         name: "1.2.1", 
-//                         parallel: true,
-//                         command: delay,
-//                         args: 600
-//                     },
-//                     {
-//                         name: "1.2.2", 
-//                         parallel: true,
-//                         command: delay,
-//                         args: 150
-//                     }
-//                 ]
-//             },
-//             {
-//                 name: "1.3",
-//                 parallel: true,
-//                 runOn: "fail",
-//                 command: delay,
-//                 args: 10,
-//             }
-//         ]
-//     },
-//     {
-//         name: "2", 
-//         parallel: true,
-//         command: delay,
-//         args: 200
-//     },
-//     {
-//         name: "3", 
-//         parallel: true,
-//         command: delay,
-//         args: 300
-//     }
-// ]);
-
 var main = new Job({ name: "main" }).add([
     {
         name: "1", 
         parallel: true,
         command: delay,
-        args: 300,
+        args: 100,
         children: [
             {
                 name: "1.1", 
                 parallel: true,
                 command: delay,
-                args: 100
+                args: 400,
+            },
+            {
+                name: "1.2",
+                parallel: true,
+                command: delay,
+                args: 50,
+                children: [
+                    {
+                        name: "1.2.1", 
+                        parallel: true,
+                        command: delay,
+                        args: 600
+                    },
+                    {
+                        name: "1.2.2", 
+                        parallel: true,
+                        command: delay,
+                        args: 150
+                    }
+                ]
+            },
+            {
+                name: "1.3",
+                parallel: true,
+                runOn: "fail",
+                command: delay,
+                args: 10,
             }
         ]
     },
@@ -84,19 +55,26 @@ var main = new Job({ name: "main" }).add([
         parallel: true,
         command: delay,
         args: 200
+    },
+    {
+        name: "3", 
+        parallel: true,
+        command: delay,
+        args: 300
     }
 ]);
-    
-main.on("monitor", function(job, eventName, data){
-    console.log(
-        "%s, %s, %s, %s, %s", 
-        job.name, 
-        eventName, 
-        job.args || "N/A", 
-        job.state,
-        JSON.stringify(data) || ""
-    );
-}).run();
+
+// main.on("monitor", function(job, eventName, data){
+//     console.log(
+//         "%s, %s, %s, %s, %s", 
+//         job.name, 
+//         eventName, 
+//         job.args || "N/A", 
+//         job.state,
+//         JSON.stringify(data) || ""
+//     );
+// });
+main.run();
 
 // main.monitor(process.stdout).run();
 
