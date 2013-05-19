9d4f3ed2ba66c4c1df1181c81a4f078c0decbbcb
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-05-19 18:54:24 +0100

    sequential seems ok

--- examples/sequential.js
+++ examples/sequential.js
@@ -4,14 +4,11 @@ var util = require("util"),
 var job = new Job({ name: "main", commandSync: String });
 var job1 = new Job({ name: "one", commandSync: String });
 var job2 = new Job({ name: "two", commandSync: String });
-var onechild1 = new Job({ name: "one child 1", commandSync: String});
+var onechild1 = new Job({ name: "one child 1", runOn: "fail", commandSync: String});
 var onechild2 = new Job({ name: "one child 2", commandSync: String});
 var twochild1 = new Job({ name: "two child 1", commandSync: String});
 var twochild2 = new Job({ name: "two child 2", commandSync: String});
 
-function log(job){
-    console.log(job.name);
-}
 job1.add(onechild1);
 job1.add(onechild2);
 job2.add(twochild1);
@@ -19,8 +16,8 @@ job2.add(twochild2);
 job.add(job1);
 job.add(job2);
 
-job.on("monitor", function(job, eventName){
-    console.log("%s, %s", job.name, eventName);
+job.on("monitor", function(job, eventName, data){
+    console.log("%s, %s, %s", job.name, eventName, JSON.stringify(data) || "");
 }).run();
 
 // job.monitor(process.stdout).run();
