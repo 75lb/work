741e72c996154e6336e430b2c4836a96b805546c
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-12-24 22:19:40 +0000

    async tests

--- test/unit-work.js
+++ test/unit-work.js
@@ -3,7 +3,7 @@ var Job = require("../lib/Job"),
     assert = require("assert"),
     l = console.log;
 
-describe("JobSync: success path", function(){
+describe("JobSync: simple success path", function(){
 
     var _job;
 
@@ -17,6 +17,10 @@ describe("JobSync: success path", function(){
             command: syncCommand,
             args: [ 1,2,3 ]
         });
+    });
+
+    it("correct name", function(){
+        assert.strictEqual(_job.name, "test");
     });
 
     it("correct state", function(){
@@ -41,5 +45,55 @@ describe("JobSync: success path", function(){
         _job.run();
         assert.ok(start);
         assert.ok(complete);
+    });
+});
+
+describe("JobSync: simple success path", function(){
+
+    var _job;
+
+    function command(){
+        var self = this,
+            argCount = arguments.length;
+        setTimeout(function(){ self.done(argCount); }, 10);
+    }
+
+    beforeEach(function(){
+        _job = new Job({
+            name: "test",
+            command: command,
+            args: [ 1,2,3,4 ]
+        });
+    });
+
+    it("correct name", function(){
+        assert.strictEqual(_job.name, "test");
+    });
+
+    it("correct state", function(){
+        assert.strictEqual(_job.state, "idle");
+        _job.run();
+        assert.strictEqual(_job.state, "running");
+    });
+
+    it("correct return value", function(done){
+        _job.run(function(ret){
+            assert.strictEqual(ret, 4);
+            done();
+        });
+    });
+
+    it("correct events", function(done){
+        var start, complete;
+        _job.on("start", function(){
+            start = true;
+        });
+        _job.on("complete", function(){
+            complete = true;
+            assert.ok(start);
+            assert.ok(complete);
+            done();
+        });
+        _job.run();
     });
 });
