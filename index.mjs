39368f0d3914d8f3691e8c1a68bdc108efa1d498
Lloyd Brookes <lloyd.brookes@gmail.com>
2020-04-18 13:41:02 +0100

    validation.. after

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
+const _maxConcurrency = new WeakMap()
+
 class Queue {
   /**
    * @param {function[]} jobs - An array of functions, each of which must return a Promise.
    * @param {number} maxConcurrency
    */
-  constructor (jobs, maxConcurrency) {
+  constructor (jobs, maxConcurrency, skipAfter) {
     this.jobs = []
     this.jobStats = {
       total: 0,
@@ -19,6 +21,18 @@ class Queue {
     for (const job of jobs) {
       this.add(job)
     }
+
+    if (!skipAfter) this.after = new Queue([], maxConcurrency, true)
+  }
+
+  get maxConcurrency () {
+    return _maxConcurrency.get(this)
+  }
+  set maxConcurrency (val) {
+    if (!Number.isInteger(val)) {
+      throw 'You must supply an integer to queue.maxConcurrency'
+    }
+    _maxConcurrency.set(this, val)
   }
 
   add (job) {
@@ -49,6 +63,9 @@ class Queue {
           yield result
         }
       }
+    }
+    if (this.after) {
+      yield * this.after
     }
   }
 
