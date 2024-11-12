6281a601bd551f781a3dfd0b9a9f9ec154b285e5
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-11-12 22:10:40 +0000

    refactor

--- dist/index.cjs
+++ dist/index.cjs
@@ -4,51 +4,60 @@ class Command {
   state
   result
 
+  /**
+   * Must be async - sync commands don't support concurrency.
+   */
   async execute () {}
 }
 
 class Queue {
-  /**
-   * @emits start
-   * @emits end
-   */
-  constructor (options = {}) {
-    this.commands = options.commands || [];
-    this.maxConcurrency = options.maxConcurrency || 1;
-    this.stats = {
-      total: this.commands.length,
-      complete: 0,
-      active: 0
-    };
+  commands = []
+  stats = {
+    total: 0,
+    complete: 0,
+    active: 0
   }
 
-  add (command) {
-    this.commands.push(command);
+  constructor (options = {}) {
+    this.maxConcurrency = options.maxConcurrency || 1;
+  }
+
+  add (command, ...args) {
+    this.commands.push({ command, args });
     this.stats.total++;
   }
 
   /**
    * Iterate over `commands` invoking no more than `maxConcurrency` at once. Yield results on receipt.
    */
+  /* TODO: yield { command, event: 'start' } or similar, rather than only yielding on completion or emitting a "start" event  */
+  /* TODO: Real-life updating of slotsAvailable if new commands are added while processing is in progress */
   async * [Symbol.asyncIterator] () {
     while (this.commands.length) {
       const slotsAvailable = this.maxConcurrency - this.stats.active;
       if (slotsAvailable > 0) {
         const toRun = [];
         for (let i = 0; i < slotsAvailable; i++) {
-          const command = this.commands.shift();
-          if (command) {
-            this.stats.active++;
-            /* TODO: yield { command, event: 'start' } or similar  */
-            const commandPromise = command.execute().then(result => {
-              this.stats.active -= 1;
-              this.stats.complete += 1;
-              return result
-            });
-            toRun.push(commandPromise);
+          const { command, args } = this.commands.shift();
+          let executable;
+          if (typeof command === 'function') {
+            executable = command;
+          } else if (typeof command === 'object' && typeof command.execute === 'function') {
+            executable = command.execute.bind(command);
+          } else {
+            throw new Error('Command structure not recognised')
           }
+          this.stats.active++;
+          const commandPromise = executable(...args).then(result => {
+            this.stats.active -= 1;
+            this.stats.complete += 1;
+            return result
+          });
+          toRun.push(commandPromise);
         }
         const completedCommands = await Promise.all(toRun);
+        // console.dir(toRun, { showHidden: true, depth: null, colors: true })
+        // console.dir(completedCommands, { showHidden: true, depth: null, colors: true })
         for (const command of completedCommands) {
           yield command;
         }
--- test/queue.js
+++ test/queue.js
@@ -100,5 +100,45 @@ test.set('iterator: maxConcurrency 3, results still in job order', async functio
   a.deepEqual(results, [1, 1.1, 1.2])
 })
 
+test.set('Accept a single async function', async function () {
+  const queue = new Queue({
+    commands: [
+      async function execute () {
+        return sleep(30, 1)
+      },
+      async function execute () {
+        return sleep(20, 1.1)
+      },
+      async function execute () {
+        return sleep(50, 1.2)
+      },
+    ],
+    maxConcurrency: 3
+  })
+
+  const results = []
+  for await (const result of queue) {
+    results.push(result)
+  }
+  a.deepEqual(results, [1, 1.1, 1.2])
+})
+
+only.set('Accept a single async function with args', async function () {
+  async function command (ms, result) {
+    return sleep(ms, result)
+  }
+
+  const queue = new Queue({ maxConcurrency: 3 })
+  queue.add(command, 30, 1)
+  queue.add(command, 20, 1.1)
+  queue.add(command, 50, 1.2)
+
+  const results = []
+  for await (const result of queue) {
+    results.push(result)
+  }
+  a.deepEqual(results, [1, 1.1, 1.2])
+})
+
 export { test, only, skip }
 
